0cb8336ae035f99f0a3e54e702496c7a6389c46c
logan moon <mseokho7733@gmail.com>
2019-08-29 15:17:43 +0900

    MemberInput.js revised

--- src/screens/MemberInput.js
+++ src/screens/MemberInput.js
@@ -1,4 +1,12 @@
 import React, {Component} from "react"
-import * from 
+import button from "../components/button"
 
-export default class MemberInput 
+export default class MemberInput extends Component {
+    render(){
+        return(
+            <div>
+                <button action {()=>()/>
+            </div>
+        )
+    }
+}
